30ad86f5226dea09ff844258994de8ad473606be
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2024-09-19 13:53:36 +0200

    deployment test

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -14,7 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <footer>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
