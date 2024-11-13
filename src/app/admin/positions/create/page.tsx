eeffba973d8f0ceda6c891dad197a0013ba1a4d6
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2024-11-13 13:23:13 +0100

    uppercase test

--- src/app/admin/positions/create/page.tsx
+++ src/app/admin/positions/create/page.tsx
@@ -1,5 +1,5 @@
 import CreatePositionForm from "@/ui/dashboard/forms/CreatePositionForm";
-import Headline from "@/ui/dashboard/typography/Headline";
+import Headline from "@/ui/dashboard/Typography/Headline";
 
 export default function CreatePage() {
   return (
