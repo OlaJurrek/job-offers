b937200491a740fb8796fae67e654e999d3fb908
Aleksandra Mętrak <aleksandra.metrak@closbrothers.pl>
2024-11-13 13:30:00 +0100

    production build test

--- src/app/admin/positions/create/page.tsx
+++ src/app/admin/positions/create/page.tsx
@@ -2,6 +2,7 @@ import CreatePositionForm from "@/ui/dashboard/forms/CreatePositionForm";
 import Headline from "@/ui/dashboard/Typography/Headline";
 
 export default function CreatePage() {
+  console.log("test");
   return (
     <>
       <Headline variant="h1">Create Position</Headline>
